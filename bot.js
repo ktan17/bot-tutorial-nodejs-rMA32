0557aff54102cdc291914dca62226d8a8e8fa4a4
ktan17 <Kevin.Tan@gpiglobal.com>
2016-06-09 21:18:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,7 +39,11 @@ function respond() {
     this.res.writeHead(200);
     var Randint = Math.floor(Math.random() * 100) + 1;
     var strRandint = "" + Randint;
+    var test = "29%";
+    var test2 = "29\%";
     postMessage(strRandint);
+    postMessage(test);
+    postMessage(test2);
     this.res.end();
   }
   else if(request.text && botRegexRules.test(request.text)) {
