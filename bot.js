bece1364ac24bc962cbb21fc016afc91ea83aa5b
ktan17 <Kevin.Tan@gpiglobal.com>
2016-06-25 15:14:41 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,7 @@ function respond() {
     var google = "www.google.com/search?q=" + query.replace(" ","+");
     
     this.res.writeHead(200);
+    postMessage(query);
     postMessage(google);
     this.res.end();
   }
